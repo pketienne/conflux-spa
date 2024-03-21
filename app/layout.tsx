cc1b96de018cf3898ac0ce3ccd44ecf18753a608
Patrick K. Etienne <git@paradox.limited>
2024-03-20 23:03:35 -0400

    Reset of project w/ Freya theme.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,23 @@
-import type { Metadata } from "next";
+'use client';
+import { LayoutProvider } from '../layout/context/layoutcontext';
+import { PrimeReactProvider } from 'primereact/api';
+import '../styles/layout/layout.scss';
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
+import 'primereact/resources/primereact.css';
+import '../styles/demo/demo.scss';
 
-export const metadata: Metadata = {};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <head>
+                <link id="theme-link" href={`/theme/theme-light/teal/theme.css`} rel="stylesheet"></link>
+            </head>
+            <body>
+                <PrimeReactProvider>
+                    <LayoutProvider>{children}</LayoutProvider>
+                </PrimeReactProvider>
+            </body>
+        </html>
+    );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	async redirects() {
-		return [
-			{
-				source: '/apps/mail',
-				destination: '/apps/mail/inbox',
-				permanent: true
-			}
-		];
-	}
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/apps/mail',
+        destination: '/apps/mail/inbox',
+        permanent: true
+      }
+    ];
+  }
 };
 
 export default nextConfig;
