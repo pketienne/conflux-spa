b33e89d88e8be8e6a7204739656d19032f22fbcf
Patrick K. Etienne <github@pketienne.me>
2024-04-04 11:46:10 -0400

    Converted sample page from server-side to client-side functionality.

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,16 @@
-// app/page.tsx
+'use client';
 
-import { revalidatePath } from 'next/cache';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import TodoList from '@/components/TodoList';
+import '@aws-amplify/ui-react/styles.css';
 
-import { AuthGetCurrentUserServer, cookiesClient } from '@/utils/amplify-utils';
-
-import Logout from '@/components/Logout';
-
-async function App() {
-	const user = await AuthGetCurrentUserServer();
-	const { data: todos } = await cookiesClient.models.Todo.list();
-
-	async function addTodo(data: FormData) {
-		'use server';
-		const title = data.get('title') as string;
-		await cookiesClient.models.Todo.create({
-			content: title,
-			done: false,
-			priority: 'medium',
-		});
-		revalidatePath('/');
-	}
-
+function App() {
 	return (
 		<>
 			<h1>Hello, Amplify 👋</h1>
-			{user && <Logout />}
-			<form action={addTodo}>
-				<input type="text" name="title" />
-				<button type="submit">Add Todo</button>
-			</form>
-
-			<ul>{todos && todos.map((todo) => <li key={todo.id}>{todo.content}</li>)}</ul>
+			<TodoList />
 		</>
 	);
 }
 
-export default App;
+export default withAuthenticator(App);
