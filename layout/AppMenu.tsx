a643f168b29806cf9e9dced65d6415ce6b67c4cc
Patrick K. Etienne <git@paradox.limited>
2024-04-06 00:09:48 -0400

    Remove non-existent `to` fields from AppMenu.

--- layout/AppMenu.tsx
+++ layout/AppMenu.tsx
@@ -9,17 +9,14 @@ const AppMenu = () => {
 				{
 					label: 'Construction Jobs',
 					icon: 'pi pi-fw pi-home',
-					to: '/dashboards/ecommerce',
 				},
 				{
 					label: 'Property Management',
 					icon: 'pi pi-fw pi-building',
-					to: '/dashboards/property-management',
 				},
 				{
 					label: 'Web Services',
 					icon: 'pi pi-fw pi-server',
-					to: '/dashboards/saas',
 				},
 			],
 		},
@@ -29,29 +26,24 @@ const AppMenu = () => {
 				{
 					label: 'Accounting',
 					icon: 'pi pi-fw pi-money-bill',
-					to: '/apps/accounting',
 					items: [
 						{
 							label: 'Chart of Accounts',
 							icon: 'pi pi-fw pi-briefcase',
-							to: '/apps/accounting/chart-of-accounts',
 						},
 						{
 							label: 'Expenses',
 							icon: 'pi pi-fw pi-dollar',
-							to: '/apps/accounting/expenses',
 						},
 						{
 							label: 'Transactions',
 							icon: 'pi pi-fw pi-dollar',
-							to: '/apps/accounting/import-transactions',
 						},
 					],
 				},
 				{
 					label: 'Calendar',
 					icon: 'pi pi-fw pi-calendar',
-					to: '/apps/calendar',
 				},
 				{
 					label: 'Contacts',
@@ -61,52 +53,42 @@ const AppMenu = () => {
 				{
 					label: 'Documents',
 					icon: 'pi pi-fw pi-file',
-					to: '/apps/documents',
 				},
 				{
 					label: 'Fleet',
 					icon: 'pi pi-fw pi-truck',
-					to: '/apps/fleet',
 				},
 				{
 					label: 'Helpdesk',
 					icon: 'pi pi-fw pi-question-circle',
-					to: '/apps/helpdesk',
 				},
 				{
 					label: 'Inventory',
 					icon: 'pi pi-fw pi-box',
-					to: '/apps/inventory',
 				},
 				{
 					label: 'Knowledge',
 					icon: 'pi pi-fw pi-book',
-					to: '/apps/knowledge',
 				},
 				{
 					label: 'Maintenance',
 					icon: 'pi pi-fw pi-wrench',
-					to: '/apps/maintenance',
 				},
 				{
 					label: 'Passwords',
 					icon: 'pi pi-fw pi-lock-open',
-					to: '/apps/projects',
 				},
 				{
 					label: 'Projects',
 					icon: 'pi pi-fw pi-check',
-					to: '/apps/projects',
 				},
 				{
 					label: 'Rentals',
 					icon: 'pi pi-fw pi-key',
-					to: '/apps/rentals',
 				},
 				{
 					label: 'Repairs',
 					icon: 'pi pi-fw pi-wrench',
-					to: '/apps/repairs',
 				},
 			],
 		},
@@ -117,7 +99,6 @@ const AppMenu = () => {
 				{
 					label: 'Documentation',
 					icon: 'pi pi-fw pi-info-circle',
-					to: '/documentation',
 				},
 			],
 		},
